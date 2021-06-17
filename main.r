544bc89b2cfe7126511110541595582b19470621
dvillaunal <dvilla@unal.edu.co>
2021-06-16 21:12:01 -0500

    Reto Diario

--- main.r
+++ main.r
@@ -1,31 +1,24 @@
-
-```{r, eval=FALSE, include=TRUE}
-"Protocolo:
- 
- 1. Daniel Felipe Villa Rengifo
- 
- 2. Lenguaje: R
- 
- 3. Tema: ANÁLISIS DISCRIMINANTE LINEAL Y CUADRÁTICO EN R [Parte 3]
- 
- 4. Fuentes:  
-    https://www.r-bloggers.com/2018/11/linear-quadratic-and-regularized-discriminant-analysis/"
-```
+## ---- eval=FALSE, include=TRUE-------------------------------------------------------
+## "Protocolo:
+## 
+##  1. Daniel Felipe Villa Rengifo
+## 
+##  2. Lenguaje: R
+## 
+##  3. Tema: ANÁLISIS DISCRIMINANTE LINEAL Y CUADRÁTICO EN R [Parte 3]
+## 
+##  4. Fuentes:
+##     https://www.r-bloggers.com/2018/11/linear-quadratic-and-regularized-discriminant-analysis/"
 
 
-```{r}
+## ------------------------------------------------------------------------------------
 # Cargamos la base de datos del replit anterior:
 Auto <- read.csv(file = "Auto.csv")
 Auto2 <- read.csv(file = "Auto2.csv")
 
-```
 
 
-# Matrices de Covarianza
-
-+ `boxM()` -> Realiza el test M de Box para determinar la homogeneidad de las matrices de covarianza obtenidas a partir de datos normales multivariados según uno o más factores de clasificación.Tiene como hipótesis nula que las matrices de covarianza son iguales. IMP: sensible a la falta de normalidad multivariante
-
-```{r}
+## ------------------------------------------------------------------------------------
 # Guardamos los OUTPUTS:
 sink("OUTPUTS.txt", split = T)
 
@@ -41,16 +34,9 @@ boxM(data = Auto2[, 2:4], grouping = Auto2[, 6])
 print("El test M de Box muestra evidencias significativas de que la matriz de covarianza no es constante para todos los grupos")
 
 print("lo que haría apropiado aplicar QDA en lugar de LDA, pero en este caso ante la falta de normalidad multivariante en los datos, el test podría haberse visto afectado por ello.")
-```
-
-# Cálculo de la función discriminante
-
-A pesar de la falta de normalidad multivariante e igualdad entre matrices de covarianza, ajustaremos los modelos `LDA` y `QDA`.
-
-1. Dividiremos el set de datos en entrenamiento __(80%)__ para ajustar el modelo, y en test __(20%)__ para evaluarlo.
 
 
-```{r}
+## ------------------------------------------------------------------------------------
 # Creamos una semilla:
 set.seed(1)
 
@@ -75,13 +61,9 @@ print(nrow(Auto.test)) #2.
 print("nrow(Auto.train) + nrow(Auto.test) == nrow(Auto)")
 print("OUTPUT:")
 print(nrow(Auto.train) + nrow(Auto.test) == nrow(Auto))
-```
 
-# Paquete `MASS`
 
-+ `lda()` -> Función para ajustar el modelo discriminante lineal
-
-```{r}
+## ------------------------------------------------------------------------------------
 # Cargar el paquete:
 #install.packages("MASS")
 library(MASS)
@@ -107,15 +89,9 @@ print(modelo.qda)
 
 # Explicación:
 "A diferencia del LDA, el QDA no contiene los coeficientes de los discriminantes lineales, puesto que el clasificador QDA se basa en una función cuadrática de los predictores, no lineal."
-```
 
-# Paquete caret
 
-+ `train()` -> Función para ajuste de modelos
-
-El paquete `caret` contiene funciones aplicadas a machine learning.
-
-```{r}
+## ------------------------------------------------------------------------------------
 # Cargamos el paquete "caret"
 #install.packages("caret")
 library(caret)
@@ -128,5 +104,3 @@ modelo.lda.caret <- train(as.factor(mpg01) ~ cylinders + displacement + horsepow
 print(modelo.lda.caret)
 
 sink()
-```
-
